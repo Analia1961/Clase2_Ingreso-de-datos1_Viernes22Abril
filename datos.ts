94b2d14bef6e1f8dfd41cc5a5d3854c3330f8396
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-04-30 23:42:48 -0300

    pruebo_IncluirComentarios agregué un título y comentarios en index.html y en datos.ts

--- datos.ts
+++ datos.ts
@@ -1,9 +1,19 @@
+// INGRESO DE DATOS
+
+/* 1-Creamos un nuevo archivo (New File) llamado “datos.ts”
+2-Escribimos en el archivo “datos.ts”: */
+
 let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("dato");
-// let rotulo = document.getElementById("rotulo");
+//let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click" , () => {
 // TU CODIGO VA AQUI, POR EJEMPLO:
 console.log("el dato ingresado es ",dato.value);
 //
 });
+
+/* Si deseamos indicar un mensaje al usuario, 
+agregamos la siguiente instrucción:
+
+rotulo.innerHTML = "Ingrese un dato: ";*/
